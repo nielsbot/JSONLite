7ba6193128495e30e597f9ef0f81bc326c45d2b2
nielsbot <github@nielsbot.com>
2014-01-16 14:38:44 -0800

    Initial Commit

---
+++ mJSON/mJSON.h
@@ -0,0 +1,13 @@
+//
+//  mJSON.h
+//  mJSON
+//
+//  Created by Niels Gabel on 1/16/14.
+//  Copyright (c) 2014 Niels Gabel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface mJSON : NSObject
+
+@end
